2ec0eddef9101885d72119ca73a22a3fbe54f188
Myke Atkinson <ploe@hotmail.co.uk>
2016-03-06 12:41:13 +0000

    Strings are variadic in fg.[Color] funcs.

--- fg/fg.go
+++ fg/fg.go
@@ -2,55 +2,75 @@ package fg
 
 import (
 	"fmt"
+	"strings"
 )
 
-func format(c int, s string) string {
-	return fmt.Sprintf("\x1b[%d;1m%s\x1b[0m", c, s)
+func format(c int, s ...string) string {
+	j := strings.Join(s, "")
+	return fmt.Sprintf("\x1b[%d;1m%s\x1b[39m", c, j)
 }
 
-var fgcode = map[string]int {
-	"black":   30,
-	"red":     31,
-	"green":   32,	
-	"yellow":  33,	
-	"blue":    34,	
-	"magenta": 35,	
-	"cyan":    36,
-	"blink":    5,
-} 
-
-// Return the string with the foreground color as Black.
-func Black (s string) string {
-	return format(fgcode["black"], s)
+var code = map[string]int {
+	"black":         30,
+	"red":           31,
+	"green":         32,	
+	"yellow":        33,	
+	"blue":          34,	
+	"magenta":       35,	
+	"cyan":          36,
+	"light gray":    37,
+	"light grey":    37,
+	"default":       39,
+	"dark gray":     90,
+	"dark grey":     90,
+	"light red":     91,
+	"light green":   92,
+	"light yellow":  93,
+	"light blue":    94,
+	"light magenta": 95,
+	"light cyan":    96,
+	"white":         97,
+	
 }
 
-// Return the string with the foreground color as Red.
-func Red (s string) string {
-	return format(fgcode["red"], s)
+// Returns the color code for the 8/16 colors.
+// s is a lowercase string.
+func Code(s string) int {
+	return code[s]
 }
 
-// Return the string with the foreground color as Green.
-func Green (s string) string {
-	return format(fgcode["green"], s)
+// Returns the strings joined and with the foreground color as Black.
+func Black (s ...string) string {
+	return format(code["black"], s...)
 }
 
-// Return the string with the foreground color as Yellow.
-func Yellow (s string) string {
-	return format(fgcode["yellow"], s)
+// Returns the strings joined and with the foreground color as Red.
+func Red (s ...string) string {
+	return format(code["red"], s...)
 }
 
-// Return the string with the foreground color as Blue.
-func Blue (s string) string {
-	return format(fgcode["blue"], s)
+// Returns the strings joined and with the foreground color as Green.
+func Green (s ...string) string {
+	return format(code["green"], s...)
 }
 
-// Return the string with the foreground color as Magenta.
-func Magenta (s string) string {
-	return format(fgcode["magenta"], s)
+// Returns the strings joined and with the foreground color as Yellow.
+func Yellow (s ...string) string {
+	return format(code["yellow"], s...)
 }
 
-// Return the string with the foreground color as Cyan.
-func Cyan (s string) string {
-	return format(fgcode["cyan"], s)
+// Returns the strings joined and with the foreground color as Blue.
+func Blue (s ...string) string {
+	return format(code["blue"], s...)
+}
+
+// Returns the strings joined and with the foreground color as Magenta.
+func Magenta (s ...string) string {
+	return format(code["magenta"], s...)
+}
+
+// Returns the strings joined and with the foreground color as Cyan.
+func Cyan (s ...string) string {
+	return format(code["cyan"], s...)
 }
 
